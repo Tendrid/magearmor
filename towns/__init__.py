60b451a9ec3daad4fab8a3ec91d0eee86715ad25
Tendrid <tendrid@gmail.com>
2021-01-16 11:05:48 -0500

    buckets

--- towns/__init__.py
+++ towns/__init__.py
@@ -321,7 +321,7 @@ class Plugin(BasePlugin):
             bukkit_chunk.getZ(), self.wilderness
         )
         if mage and not self.check_town_permission(mage, town, "build"):
-            event.setBuildable(False)
+            event.setCancelled(True)
             raise PlayerErrorMessage(
                 "You do not have permission to build in {}".format(town.name)
             )
